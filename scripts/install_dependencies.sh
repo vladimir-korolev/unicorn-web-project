0173c9bd8b848709af0377d4aefa72e52b72f4d2
Uladzimir_Karaleu <uladzimir_karaleu@epam.com>
2025-07-15 11:00:28 +0300

    Adding code deploy files

---
+++ scripts/install_dependencies.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# sudo chown tomcat:tomcat /opt/tomcat/webapps/unicorn-web-project.war
+
+
+sudo yum install tomcat -y
+sudo yum -y install httpd
+sudo cat << EOF > /etc/httpd/conf.d/tomcat_manager.conf
+<VirtualHost *:80>
+    ServerAdmin root@localhost
+    ServerName app.wildrydes.com
+    DefaultType text/html
+    ProxyRequests off
+    ProxyPreserveHost On
+    ProxyPass / http://localhost:8080/unicorn-web-project/
+    ProxyPassReverse / http://localhost:8080/unicorn-web-project/
+</VirtualHost>
+EOF
